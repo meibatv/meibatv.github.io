c0f444f51d9255b0f9af62a50fc5b09661c42caa
meibatv <57975321+meibatv@users.noreply.github.com>
2019-11-20 11:50:35 +0800

    tj.js
    tj.js

---
+++ tj.js
@@ -0,0 +1,47 @@
+function checkurl(num){
+    if (num===1){
+        var strU = "https";
+        strU += "://";
+        strU += "";
+        var strU2 =  "meiba5a@bcom";
+        strU2 = strU2.replace(/a@b/g,'.');
+        strU += strU2;
+        window.location.href=strU;
+    }
+    if (num===2){
+        var strU = "https";
+        strU += "://";
+        strU += "";
+        var strU2 =  "meiba5a@bcom";
+        strU2 = strU2.replace(/a@b/g,'.');
+        strU += strU2;
+        window.location.href=strU;
+    }
+    if (num===3){
+        var strU = "https";
+        strU += "://";
+        strU += "";
+        var strU2 =  "meiba5a@bcom";
+        strU2 = strU2.replace(/a@b/g,'.');
+        strU += strU2;
+        window.location.href=strU;
+    }
+    if (num===4){
+        var strU = "https";
+        strU += "://";
+        strU += "";
+        var strU2 =  "meiba5a@bcom";
+        strU2 = strU2.replace(/a@b/g,'.');
+        strU += strU2;
+        window.location.href=strU;
+    }
+    if (num===5){
+        var strU = "https";
+        strU += "://";
+        strU += "";
+        var strU2 =  "meiba5a@bcom";
+        strU2 = strU2.replace(/a@b/g,'.');
+        strU += strU2;
+        window.location.href=strU;
+    }
+}
